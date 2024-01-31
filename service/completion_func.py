1b2ccea23ca77e4e6521e2f6c95c06f884364547
sevel.liu <sevel.liu@cloudminds.com>
2024-01-31 11:46:30 +0800

    1.fix(llm interface)

--- service/completion_func.py
+++ service/completion_func.py
@@ -5,9 +5,10 @@ import asyncio
 import typing as t
 
 import httpx
+from jsonpath import jsonpath
 from dotenv import load_dotenv, find_dotenv
 from langchain_core.callbacks import Callbacks
-from langchain_core.outputs import LLMResult
+from langchain_core.outputs import LLMResult, Generation
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 
 from ragas.llms.base import BaseRagasLLM
@@ -15,6 +16,18 @@ from ragas.llms.prompt import PromptValue
 
 _ = load_dotenv(find_dotenv())
 _authorization = os.getenv("OpenAPIKey")
+
+
+def get_prompt_json(prompt: PromptValue):
+    messages = []
+    for row in prompt.to_messages():
+        if isinstance(row, SystemMessage):
+            messages.append({"role": "system", "content": row.content})
+        elif isinstance(row, HumanMessage):
+            messages.append({"role": "user", "content": row.content})
+        elif isinstance(row, AIMessage):
+            messages.append({"role": "assistant", "content": row.content})
+    return messages
 
 
 class GatewayLLM(BaseRagasLLM):
@@ -36,6 +49,15 @@ class GatewayLLM(BaseRagasLLM):
         self.top_p = top_p
         self.repetition_penalty = repetition_penalty
 
+    @property
+    def _headers(self):
+        return {
+            "Content-Type": "application/json; charset=utf-8",
+            "ApiName": "robotGptLLMApi",
+            "Model": self.model,
+            "Authorization": _authorization
+        }
+
     def generate_text(
             self,
             prompt: PromptValue,
@@ -44,7 +66,39 @@ class GatewayLLM(BaseRagasLLM):
             stop: t.Optional[t.List[str]] = None,
             callbacks: Callbacks = [],
     ) -> LLMResult:
-        ...
+        request_json = {
+            "model": self.model,
+            "temperature": temperature,
+            "n": n,
+            "top_p": self.top_p,
+            "repetition_penalty": self.repetition_penalty,
+            "max_tokens": self.max_tokens,
+            "max_new_token": self.max_tokens,
+            "api_timeout": self.api_timeout,
+            "do_sample": self.do_sample,
+            "stream": False,
+            "stop": stop,
+            "messages": get_prompt_json(prompt),
+            "trace_id": f"RAGAS{uuid.uuid4().hex}@cloudminds-test.com.cn"
+        }
+
+        try:
+            with httpx.Client(verify=False) as client:
+                response = client.post(
+                    self.address,
+                    headers=self._headers,
+                    json=request_json,
+                    timeout=self.api_timeout,
+                )
+                response.raise_for_status()
+                ret = response.json()
+                return LLMResult(generations=[[Generation(text="".join(jsonpath(ret, "$.choices..message.content")))]])
+        except httpx.HTTPStatusError as e:
+            print(f"HTTP request failed with status {e.response.status_code}.")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+        return LLMResult(generations=[[Generation(text="")]])
 
     async def agenerate_text(
             self,
@@ -54,59 +108,63 @@ class GatewayLLM(BaseRagasLLM):
             stop: t.Optional[t.List[str]] = None,
             callbacks: Callbacks = [],
     ) -> LLMResult:
+        request_json = {
+            "model": self.model,
+            "temperature": temperature,
+            "n": n,
+            "top_p": self.top_p,
+            "repetition_penalty": self.repetition_penalty,
+            "max_tokens": self.max_tokens,
+            "max_new_token": self.max_tokens,
+            "api_timeout": self.api_timeout,
+            "do_sample": self.do_sample,
+            "stream": True,
+            "stop": stop,
+            "messages": get_prompt_json(prompt),
+            "trace_id": f"RAGAS{uuid.uuid4().hex}@cloudminds-test.com.cn"
+        }
+
         completions = []
-        messages = []
-        for row in prompt.to_messages():
-            if isinstance(row, SystemMessage):
-                messages.append({"role": "system", "content": SystemMessage.content})
-            if isinstance(row, HumanMessage):
-                messages.append({"role": "user", "content": HumanMessage.content})
-            if isinstance(row, AIMessage):
-                messages.append({"role": "assistant", "content": AIMessage.content})
-        async with httpx.AsyncClient(verify=False) as client:
-            try:
-                headers = {
-                    "Content-Type": "application/json; charset=utf-8",
-                    "ApiName": "robotGptLLMApi",
-                    "Model": self.model,
-                    "Authorization": _authorization
-                }
+        try:
+            async with httpx.AsyncClient(verify=False) as client:
                 async with client.stream(
                         "POST",
                         self.address,
-                        headers=headers,
-                        json={
-                            "model": self.model,
-                            "temperature": temperature,
-                            "n": n,
-                            "top_p": self.top_p,
-                            "repetition_penalty": self.repetition_penalty,
-                            "max_tokens": self.max_tokens,
-                            "max_new_token": self.max_tokens,
-                            "api_timeout": self.api_timeout,
-                            "do_sample": self.do_sample,
-                            "stream": True,
-                            "stop": stop,
-                            "messages": prompt.to_messages(),
-                            "trace_id": f"RAGAS{uuid.uuid4().hex}@cloudminds-test.com.cn"
-                        },
+                        headers=self._headers,
+                        json=request_json,
                         timeout=self.api_timeout,
                 ) as response:
+                    response.raise_for_status()
                     async for chunk in response.aiter_text():
                         chunk = chunk.strip() if isinstance(chunk, str) else chunk
                         if not chunk:
                             continue
                         data = json.loads(chunk.removeprefix("data:"))
-                        print(data)
-            except Exception as e:
-                print(e)
+                        for choice in data["choices"]:
+                            if choice["finish_reason"] in ["stop"]:
+                                return LLMResult(generations=[[Generation(text="".join(completions))]])
+                            if "delta" in choice:
+                                completions.append(choice["delta"]["content"])
+                            elif "message" in choice:
+                                completions.append(choice["message"]["content"])
+        except httpx.HTTPStatusError as e:
+            print(f"HTTP request failed with status {e.response.status_code}.")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+        return LLMResult(generations=[[Generation(text="".join(completions))]])
 
 
 async def main():
-    # https://dataai.harix.iamidata.com/llm/api/ask
-    llm = GatewayLLM(model="robotgpt-qw-1dot8")
+    llm = GatewayLLM(
+        # address="https://dataai.harix.iamidata.com/llm/api/ask",
+        # model="zhipuai/glm-4",
+        model="robotgpt-rc-trt-pcache",
+    )
     p = PromptValue(prompt_str="你好")
-    llm_result = await llm.agenerate_text(prompt=p)
+    llm_result = llm.generate_text(prompt=p, temperature=0.1)
+    print(llm_result)
+    llm_result = await llm.agenerate_text(prompt=p, temperature=0.1)
     print(llm_result)
 
 
